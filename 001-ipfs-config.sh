ce7fa3a940f3f1137efeaf802ecf9e76e03a29e1
rupeshgyawali <rupesh@ngit.com.np>
2025-02-04 16:04:26 +0545

    feat: update compose

---
+++ 001-ipfs-config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -ex
+ipfs bootstrap rm all
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["GET", "POST", "OPTIONS"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
